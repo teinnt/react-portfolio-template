4a0f1f30aaa38ced11ca9077235170aff6c15c20
teinnt <61264574+teinnt@users.noreply.github.com>
2024-05-30 22:33:01 +1200

    Fix with them

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,17 +3,7 @@ import { useRouter } from 'next/router'
 import { Popover } from '@headlessui/react'
 import Button from '../Button'
 import data from '../../data/portfolio.json'
-
-const productPopups = [
-  {
-    name: '1',
-    url: '/product/1',
-  },
-  {
-    name: '2',
-    url: '/product/2',
-  },
-]
+import { useTheme } from 'next-themes'
 
 interface HeaderProps {
   handleAboutScroll?: () => void
@@ -28,11 +18,12 @@ const Header: FC<HeaderProps> = ({
 }) => {
   const router = useRouter()
   const [languague, setLanguague] = useState<'VN' | 'EN'>('EN')
-  const { name, showBlog, showResume } = data
+  const { name } = data
+  const { theme } = useTheme()
 
   return (
     <>
-      <Popover className="block tablet:hidden mt-5">
+      <Popover className="block tablet:hidden">
         {({ open }) => (
           <>
             <div className="flex items-center justify-between p-2 laptop:p-0">
@@ -43,32 +34,125 @@ const Header: FC<HeaderProps> = ({
                 {name}.
               </h1>
               <div className="flex items-center">
-                {data.darkMode && (
-                  <Button
-                    onClick={() =>
-                      setLanguague(languague === 'VN' ? 'EN' : 'VN')
-                    }
-                  >
-                    {languague}
-                  </Button>
-                )}
+                <Button
+                  type="primary"
+                  onClick={() => setLanguague(languague === 'VN' ? 'EN' : 'VN')}
+                >
+                  {languague}
+                </Button>
                 <Popover.Button>
                   <img
                     className="h-5"
-                    src={`/images/${!open ? 'cancel.svg' : 'cancel-white.svg'}`}
+                    src={`/images/${!open ? 'menu.svg' : 'cancel.svg'}`}
                     alt="Menu Button"
                   />
                 </Popover.Button>
               </div>
             </div>
             <Popover.Panel
-              className={`absolute right-0 z-10 w-11/12 p-4 ${'bg-white'} shadow-md rounded-md`}
-            ></Popover.Panel>
+              className={`absolute right-0 z-10 w-11/12 p-4 bg-slate-800 shadow-md rounded-md`}
+            >
+              {!isBlog ? (
+                <div className="grid grid-cols-1">
+                  <div style={{ marginInline: '0.2rem' }}>
+                    <Popover>
+                      <Popover.Button
+                        className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
+                          data.showCursor ? 'cursor-none' : ''
+                        } ${theme === 'dark' ? 'bg-white text-black' : 'bg-black text-white'}`}
+                        style={{ width: '100%' }}
+                      >
+                        Product
+                      </Popover.Button>
+
+                      <Popover.Panel className="absolute z-10">
+                        <div
+                          style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: '10px',
+                            padding: '10px',
+                          }}
+                        >
+                          <a href="/product/one">Product One</a>
+                          <a href="/product/two">Product Two</a>
+                        </div>
+                      </Popover.Panel>
+                    </Popover>
+                  </div>
+
+                  <Button type={'primary'} onClick={() => router.push('/blog')}>
+                    Blog
+                  </Button>
+
+                  <Button type={'primary'} onClick={handleAboutScroll}>
+                    About
+                  </Button>
+                  <Button type={'primary'} onClick={handleContactScroll}>
+                    Contact
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      setLanguague(languague === 'VN' ? 'EN' : 'VN')
+                    }
+                  >
+                    <div className="h-6">{languague}</div>
+                  </Button>
+                </div>
+              ) : (
+                <div className="grid grid-cols-1">
+                  <div style={{ marginInline: '0.5em' }}>
+                    <Popover>
+                      <Popover.Button
+                        className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
+                          data.showCursor ? 'cursor-none' : ''
+                        } ${theme === 'dark' ? 'bg-white text-black' : 'bg-black text-white'}`}
+                        style={{ width: '100%' }}
+                      >
+                        Product
+                      </Popover.Button>
+
+                      <Popover.Panel className="absolute z-10">
+                        <div
+                          style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: '10px',
+                            padding: '10px',
+                          }}
+                        >
+                          <a href="/product/one">Product One</a>
+                          <a href="/product/two">Product Two</a>
+                        </div>
+                      </Popover.Panel>
+                    </Popover>
+                  </div>
+
+                  <Button type={'primary'} onClick={() => router.push('/blog')}>
+                    Blog
+                  </Button>
+
+                  <Button type={'primary'} onClick={handleAboutScroll}>
+                    About
+                  </Button>
+                  <Button type={'primary'} onClick={handleContactScroll}>
+                    Contact
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      setLanguague(languague === 'VN' ? 'EN' : 'VN')
+                    }
+                  >
+                    <div className="h-6">{languague}</div>
+                  </Button>
+                </div>
+              )}
+            </Popover.Panel>
           </>
         )}
       </Popover>
       <div
-        className={`mt-10 hidden flex-row items-center justify-between sticky dark:text-white top-0 z-10 tablet:flex`}
+        className={`hidden flex-row items-center justify-between sticky dark:text-white top-0 z-10 tablet:flex`}
         style={{ width: '100vw', paddingInline: '5%' }}
       >
         <h1
@@ -83,9 +167,16 @@ const Header: FC<HeaderProps> = ({
             alignItems: 'center',
           }}
         >
-          <div style={{ marginInline: '1em' }}>
+          <div style={{ marginInline: '0.5em' }}>
             <Popover>
-              <Popover.Button>Product</Popover.Button>
+              <Popover.Button
+                className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
+                  data.showCursor ? 'cursor-none' : ''
+                } ${theme === 'dark' ? 'bg-white text-black' : 'bg-black text-white'}`}
+                style={{ width: '100%' }}
+              >
+                Product
+              </Popover.Button>
 
               <Popover.Panel className="absolute z-10">
                 <div
@@ -103,11 +194,18 @@ const Header: FC<HeaderProps> = ({
             </Popover>
           </div>
 
-          <Button onClick={() => router.push('/blog')}>Blog</Button>
+          <Button type={'primary'} onClick={() => router.push('/blog')}>
+            Blog
+          </Button>
 
-          <Button onClick={handleAboutScroll}>About</Button>
-          <Button onClick={handleContactScroll}>Contact</Button>
+          <Button type={'primary'} onClick={handleAboutScroll}>
+            About
+          </Button>
+          <Button type={'primary'} onClick={handleContactScroll}>
+            Contact
+          </Button>
           <Button
+            type="primary"
             onClick={() => setLanguague(languague === 'VN' ? 'EN' : 'VN')}
           >
             <div className="h-6">{languague}</div>
